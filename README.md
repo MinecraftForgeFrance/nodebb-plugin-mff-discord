564493c4c27f97823fc33bfbb1bc04b79fe7da0f
robin4002 <robin4002@gmail.com>
2018-05-02 17:33:01 +0200

    Change register route to post, add licence and update readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodebb-plugin-mff-discord
+
+A NodeBB plugin to connect the MFF forum with our Discord. Designed for our needs.
+
+## Features:
+* Send mp with a code to link users discord account with they forum account.
+* Search for tutorials or solved help request.
+* Link the shoutbox with a discord chanel.
+
+
+This plugin won't be published on npm, this repo is just here to share our code ;)
--- index.js
+++ index.js
@@ -4,46 +4,68 @@ const plugin = {};
 
 plugin.init = (params, callback) => {
     let app = params.router;
-    // TODO : Le token pose problème
-    /*app.use((req, res, next) => {
-        if(req.query.token && req.query.token === "XPcTxMfU9XKBhpLGHDdeHBaxXvD3vJSdqVP72cv4FH8Pjrbv3VxZyZ77abLKtHKeK7TH45pNMnSFKgVtLe2Su4T6") {
-            next();
-        }
-        res.status(403);
-    });*/
-    app.get("/discord/register", renderRegister);
+    app.post("/discordapi/register", generateAndSendCode);
+    app.get("/discordapi/tutorial", getTutorial);
+    app.get("/discordapi/solvedthread", getSolvedThread);
     callback();
 };
 
-renderRegister = (req, res, next) => {
-    if (req.query.username !== "undefined") {
-        user.getUidByUsername(req.query.username, (err, result) => {
-            if (!err) {
-                console.log(result);
-                if (result > 1) {
-                    const preMessage = "Bot Minecraft Forge France\n";
-                    const message = "Voici votre token d'accès au Discord de Minecraft Forge France : ";
-                    const postMessage = "\nSi vous n'avez pas fait de demande de code d'accès, merci de le signalez à l'équipe de Minecraft Forge France.";
-                    const randomNumber = randomizeNumber();
-                    socketModule.chats.newRoom({uid: 1}, {touid: result}, (err, roomId) => {
-                        socketModule.chats.send({uid: 1}, {
-                            roomId: roomId,
-                            message: preMessage + message + randomNumber + postMessage
-                        }, (err, messageData) => {
-                            if(err)
-                                throw err;
+generateAndSendCode = (req, res, next) => {
+    // TODO load token from config file
+    if(req.body.token && req.body.token === "XPcTxMfU9XKBhpLGHDdeHBaxXvD3vJSdqVP72cv4FH8Pjrbv3VxZyZ77abLKtHKeK7TH45pNMnSFKgVtLe2Su4T6") {
+        if (req.body.username) {
+            user.getUidByUsername(req.body.username, (err, result) => {
+                if (!err) {
+                    if (result > 1) {
+                        let randomNumber = randomizeNumber();
+                        socketModule.chats.newRoom({uid: 1}, {touid: result}, (err2, roomId) => {
+                            if(!err2) {
+                                socketModule.chats.send({uid: 1}, {
+                                    roomId: roomId,
+                                    message: `Voici votre token d'accès au Discord de Minecraft Forge France : ${randomNumber}.
+                                    Si vous n'avez pas fait de demande de code d'accès, merci de le signalez à l'équipe de Minecraft Forge France.`
+                                }, (err3, messageData) => {
+                                    if(err3) {
+                                        console.error("Couldn't send message: " + err3);
+                                        res.status(500).json({error: "Failed to send a private message"});
+                                    }
+                                    else {
+                                        res.json({result: randomNumber});
+                                    }
+                                });
+                            }
+                            else {
+                                console.error("Couldn't create chat room: " + err2);
+                                res.status(500).json({error: "Failed to create chat room"});
+                            }
                         });
-                        res.json({result: randomNumber});
-                    });
+                    }
+                    else {
+                        res.status(200).json({error: "User not found"});
+                    }
                 }
-                else
-                    res.json({error: "User not found"});
-            }
-            else
-                throw err;
-        });
+                else {
+                    console.error("Couln't find user with name :" + req.body.username + ", err: " + err);
+                    res.status(500).json({error: "Couln't get id of this user"});
+                }
+            });
+        }
+        else {
+            res.status(400).json({error: "Missing arguments"});
+        }
+    }
+    else {
+        res.status(403).json({error: "Invalid token!"});
     }
 };
+
+function getTutorial(req, res, next) {
+
+}
+
+function getSolvedThread(req, res, next) {
+
+}
 
 randomizeNumber = () => {
     let number = "";
