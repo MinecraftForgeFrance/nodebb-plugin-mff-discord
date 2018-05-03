128dea5de824aff4f6691eb68f18ea2638cf0d8b
Robin <robin4002@gmail.com>
2018-05-03 23:10:38 +0200

    Add all route, start config

--- index.js
+++ index.js
@@ -1,70 +1,117 @@
 const user = module.parent.require("./user");
 const socketModule = module.parent.require("./socket.io/modules");
-const plugin = {};
+const meta = module.parent.require('./meta');
 
-plugin.init = (params, callback) => {
-    let app = params.router;
-    app.post("/discordapi/register", generateAndSendCode);
-    app.get("/discordapi/tutorial", getTutorial);
-    app.get("/discordapi/solvedthread", getSolvedThread);
-    callback();
+
+const MFFDiscordBridge = {
+    token: "changeme",
+    discordWebHook: "changeme",
+    // init the plugin
+    init: function (params, callback) {
+        let app = params.router;
+        let middleware = params.middleware;
+    
+        // discord bridge api
+        app.post("/discordapi/register", checkToken, generateAndSendCode);
+        app.get("/discordapi/tutorial", checkToken, getTutorial);
+        app.get("/discordapi/solvedthread", checkToken, getSolvedThread);
+        
+        // admin panel
+        app.get('/admin/plugins/mff-discord', middleware.admin.buildHeader, renderAdmin);
+        app.get('/api/admin/plugins/mff-discord', renderAdmin);
+    
+        meta.settings.get('mffdiscordbridge', function (err, options) {
+			if (err) {
+				winston.warn('[plugin/mffdiscordbridge] Unable to retrieve settings, will keep defaults: ' + err.message);
+            }
+            else {
+                // load value from config if exist, keep default otherwise
+                if (options.hasOwnProperty("token")) {
+                    MFFDiscordBridge.token = options["token"];
+                }
+
+                if (options.hasOwnProperty("webhook")) {
+                    MFFDiscordBridge.discordWebHook = options["webhook"];
+                }
+            }
+        });
+
+        callback();
+    },
+    // append mffdiscordbridge config to nodebb
+    appendConfig: function (config, callback) {
+        config.mffdiscordbridge = {
+            token: MFFDiscordBridge.token,
+            webhook: MFFDiscordBridge.discordWebHook,
+        };
+        callback(null, config);
+    }
 };
 
-generateAndSendCode = (req, res, next) => {
-    // TODO load token from config file
-    if(req.body.token && req.body.token === "XPcTxMfU9XKBhpLGHDdeHBaxXvD3vJSdqVP72cv4FH8Pjrbv3VxZyZ77abLKtHKeK7TH45pNMnSFKgVtLe2Su4T6") {
-        if (req.body.username) {
-            user.getUidByUsername(req.body.username, (err, result) => {
-                if (!err) {
-                    if (result > 1) {
-                        let randomNumber = randomizeNumber();
-                        socketModule.chats.newRoom({uid: 1}, {touid: result}, (err2, roomId) => {
-                            if(!err2) {
-                                socketModule.chats.send({uid: 1}, {
-                                    roomId: roomId,
-                                    message: `Voici votre token d'accès au Discord de Minecraft Forge France : ${randomNumber}.
-                                    Si vous n'avez pas fait de demande de code d'accès, merci de le signalez à l'équipe de Minecraft Forge France.`
-                                }, (err3, messageData) => {
-                                    if(err3) {
-                                        console.error("Couldn't send message: " + err3);
-                                        res.status(500).json({error: "Failed to send a private message"});
-                                    }
-                                    else {
-                                        res.json({result: randomNumber});
-                                    }
-                                });
-                            }
-                            else {
-                                console.error("Couldn't create chat room: " + err2);
-                                res.status(500).json({error: "Failed to create chat room"});
-                            }
-                        });
-                    }
-                    else {
-                        res.status(200).json({error: "User not found"});
-                    }
-                }
-                else {
-                    console.error("Couln't find user with name :" + req.body.username + ", err: " + err);
-                    res.status(500).json({error: "Couln't get id of this user"});
-                }
-            });
-        }
-        else {
-            res.status(400).json({error: "Missing arguments"});
-        }
+// check token middleware
+function checkToken(req, res, next) {
+    let token = req.body.token || req.query.token || "";
+    if(token === MFFDiscordBridge.token) {
+        next();
     }
     else {
         res.status(403).json({error: "Invalid token!"});
     }
-};
-
-function getTutorial(req, res, next) {
-
 }
 
-function getSolvedThread(req, res, next) {
+function generateAndSendCode(req, res) {
+    if (req.body.username) {
+        user.getUidByUsername(req.body.username, (err, result) => {
+            if (!err) {
+                if (result > 1) {
+                    let randomNumber = randomizeNumber();
+                    socketModule.chats.newRoom({uid: 1}, {touid: result}, (err2, roomId) => {
+                        if(!err2) {
+                            socketModule.chats.send({uid: 1}, {
+                                roomId: roomId,
+                                message: `Voici votre token d'accès au Discord de Minecraft Forge France : ${randomNumber}.
+                                Si vous n'avez pas fait de demande de code d'accès, merci de le signalez à l'équipe de Minecraft Forge France.`
+                            }, (err3, messageData) => {
+                                if(err3) {
+                                    console.error("Couldn't send message: " + err3);
+                                    res.status(500).json({error: "Failed to send a private message"});
+                                }
+                                else {
+                                    res.json({result: randomNumber});
+                                }
+                            });
+                        }
+                        else {
+                            console.error("Couldn't create chat room: " + err2);
+                            res.status(500).json({error: "Failed to create chat room"});
+                        }
+                    });
+                }
+                else {
+                    res.status(200).json({error: "User not found"});
+                }
+            }
+            else {
+                console.error("Couln't find user with name :" + req.body.username + ", err: " + err);
+                res.status(500).json({error: "Couln't get id of this user"});
+            }
+        });
+    }
+    else {
+        res.status(400).json({error: "Missing arguments"});
+    }
+};
 
+function getTutorial(req, res) {
+    res.status(200).json({msg: "Not implemented for now"});
+}
+
+function getSolvedThread(req, res) {
+    res.status(200).json({msg: "Not implemented for now"});
+}
+
+function renderAdmin(req, res) {
+    res.render('admin/plugins/mff-discord');
 }
 
 randomizeNumber = () => {
@@ -81,4 +128,4 @@ getRandomIntInclusive = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-module.exports = plugin;
+module.exports = MFFDiscordBridge;
